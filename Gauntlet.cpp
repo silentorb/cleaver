e9dca2a50149a819c9b3093404f22d88167bb06c
cj <christopherwjohnson@gmail.com>
2015-08-20 00:19:33 -0600

    First commit.

---
+++ Gauntlet.cpp
@@ -0,0 +1,5 @@
+#include "Gauntlet.h"
+
+namespace gauntlet {
+
+}
---
+++ Gauntlet.h
@@ -0,0 +1,14 @@
+#ifndef GAUNTLET_GAUNTLET_H
+#define GAUNTLET_GAUNTLET_H
+
+#include "Ogre.h"
+
+namespace gauntlet {
+
+  class Gauntlet {
+
+  };
+
+}
+
+#endif //GAUNTLET_GAUNTLET_H
